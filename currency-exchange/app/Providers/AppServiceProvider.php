5a00c03c595be1de2cff7639933213066d75f3c8
Chen <131841805+ChenY6688@users.noreply.github.com>
2024-06-22 20:54:18 +0800

    set currency_exchange dependency injection

--- currency-exchange/app/Providers/AppServiceProvider.php
+++ currency-exchange/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Services\CurrencyExchangeService;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,25 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton(CurrencyExchangeService::class, function ($app) {
+            return new CurrencyExchangeService([
+                "TWD" => [
+                    "TWD" => 1,
+                    "JPY" => 3.669,
+                    "USD" => 0.03281
+                ],
+                "JPY" => [
+                    "TWD" => 0.26956,
+                    "JPY" => 1,
+                    "USD" => 0.00885
+                ],
+                "USD" => [
+                    "TWD" => 30.444,
+                    "JPY" => 111.801,
+                    "USD" => 1
+                ]
+            ]);
+        });
     }
 
     /**
